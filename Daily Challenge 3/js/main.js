a2b06fe7dc25d9b52e6be7de44409e8760f4c5e1
jackie <jdimery2022@gmail.com>
2022-07-05 23:37:39 -0700

    recovered first commit

---
+++ Daily Challenge 3/js/main.js
@@ -0,0 +1,21 @@
+
+const answer = popups.prompt;
+console.log(answer);
+
+
+
+
+// CHALLENGE 1
+// Prompt the user for their first name //
+// Store their input to a variable //
+
+// reverse your user's name
+// alert the reversed string
+
+
+// Challenge 2
+// Prompt for a number value (provide a default of 10)
+// Prompt for a second number value (provide a default of 10)
+// Convert the prompted values into integers using parseInt()
+// Add the numbers together and alert the user with the result
+
