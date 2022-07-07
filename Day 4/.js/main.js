56286e3801b09158c0c203471ecd6f71d0e14bde
jackie <jdimery2022@gmail.com>
2022-07-07 11:47:32 -0700

    completed take home challenge for Day 4

--- Day 4/.js/main.js
+++ Day 4/.js/main.js
@@ -7,7 +7,7 @@
 
 
 
-    // ------------ TAKE HOME CHALLENGE: STILL NEED TO COMPLETE ------------
+    // ------------ TAKE HOME CHALLENGE: COMPLETED ON 7/7/2022 ------------
     // Add an edge case to your daily challenge code
     // Check if the user input anything other than a number
     // If so, prompt the user again to input a number
