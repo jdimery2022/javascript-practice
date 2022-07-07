602847ba858d4e7f2396638c46af45e63c220cff
jackie <jdimery2022@gmail.com>
2022-07-07 11:31:13 -0700

    organize folder repo

---
+++ Day 4/.js/main.js
@@ -0,0 +1,13 @@
+    //-------------DAILY CHALLENGE : COMPLETED ON 7/6/2022------------
+    // Prompt the user for two different numbers
+    // Convert the values to integers
+    // Store the two integers as variables
+    // Compare the two numbers
+    // Alert the larger number
+
+
+
+    // ------------ TAKE HOME CHALLENGE: STILL NEED TO COMPLETE ------------
+    // Add an edge case to your daily challenge code
+    // Check if the user input anything other than a number
+    // If so, prompt the user again to input a number
