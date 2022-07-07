66e3e5ca006cb2f93375fd0e3fd179780b1cd5a3
jackie <jdimery2022@gmail.com>
2022-07-07 14:51:28 -0700

    completed day 5 (in class) challenge

---
+++ Day 5/.js/main.js
@@ -0,0 +1,13 @@
+//---Daily Challenge COMPLETED ON 7/7/2022 BEFORE CLASS----
+// Create a new codepen
+// Write a function that accepts a number as an argument
+// Alert the user whether the given number is even or odd
+// Call your function
+
+
+// Take Home Challenge:  NEED TO COMPLETE
+// Write a function that accepts a password (string)
+// Validate the password using the following rules:
+// Must be 6-20 characters long
+// Must start with a letter
+// Alert the user if their password has been accepted or why their password was rejected
