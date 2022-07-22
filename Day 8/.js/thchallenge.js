143815fd9c2a4bb5a11c0a7dfc0f8b2c00e78405
jackie <jdimery2022@gmail.com>
2022-07-21 21:29:29 -0700

    commit magic.zip,

---
+++ Day 8/.js/thchallenge.js
@@ -0,0 +1,8 @@
+// Take Home Challenge
+// Create a new codepen:
+// Let's say you're a car manufacturer and need to create many cars to build your inventory
+// Create a class called "car"
+// Add some properties like "make", "model", "year", "color", etc
+// Use your car class to construct 4 (or more) different cars
+// console log your code so you can see the results!
+// BONUS: Display the details for each of your cars in your HTML
